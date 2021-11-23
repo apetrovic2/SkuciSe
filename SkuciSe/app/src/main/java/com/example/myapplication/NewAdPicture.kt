3fbba4664123f1aa39b9b7da2cba249d8843ac73
ana.petrovic2 <apetrovicpmf@gmail.com>
2021-11-23 02:46:15 +0100

    Stranice za dodavanje slike prilikom registracije i dodavanje slike prilikom dodavanja oglasa

---
+++ SkuciSe/app/src/main/java/com/example/myapplication/NewAdPicture.kt
@@ -0,0 +1,11 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class NewAdPicture : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_ad_picture)
+    }
+}
